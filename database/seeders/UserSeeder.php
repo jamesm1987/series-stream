a31d76fdb166f3c1c5fd9cc4d1622941628e94b6
James McCormack <james@wirebox.co.uk>
2021-12-22 21:57:22 +0100

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Database\Seeder;
@@ -27,7 +28,20 @@ class UserSeeder extends Seeder
 
         $user->save();
 
-        $role = Role::create(['name' => 'Super-Admin']);
+        DB::table('model_has_roles')->insert([
+            'role_id' => 1,
+            'model_id' => 1,
+            'model_type' => 'App\Models\User',
+            'team_id' => 1
+        ]);
+
+        $role = Role::create([
+            'name' => 'admin', 
+            'team_id' => 1, 
+            'guard_name' => 'web', 
+            'created_at' => NOW(), 
+            'updated_at' => NOW()
+        ]);
         $user->assignRole($role);
 
 
