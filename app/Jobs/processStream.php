aef6f33ec5dc51df7d06c107d14d449376d6675f
James McCormack <james@wirebox.co.uk>
2021-12-25 16:33:22 +0100

    process job debug

--- app/Jobs/processStream.php
+++ app/Jobs/processStream.php
@@ -42,59 +42,67 @@ class processStream implements ShouldQueue
         //The path & filename to save to.
         $saveTo = $this->uploadsDir . '/' . Str::uuid() . '.mp4';
 
-        //Open file handler.
-        $fp = fopen($saveTo, 'w+');
+        $file = file_put_contents($saveTo, $this->upload);
 
-        //If $fp is FALSE, something went wrong.
-        if ($fp === false) {
-            Log::debug('Could not open: '.$saveTo);
-        }
-        
-        //Create a cURL handle.
-        $ch = curl_init($this->upload);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
-
-        curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: curl/7.39.0');
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        
-
-        //Pass our file handle to cURL.
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-
-        //Timeout if the file doesn't download after 8mins.
-        curl_setopt($ch, CURLOPT_TIMEOUT, 480);
-
-        //Execute the request.
-        curl_exec($ch);
-
-        //If there was an error, throw an Exception
-        if (curl_errno($ch)) {
-            Log::debug(curl_error($ch));
-        }
-
-        //Get the HTTP status code.
-        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $statusText = curl_getinfo($ch);
-
-        //Close the cURL handler.
-        curl_close($ch);
-
-        //Close the file handler.
-        fclose($fp);
-
-        if ($statusCode == 200) {
+        if ($file) {
             $this->stream->update([
                 'url' => $saveTo
             ]);
-        } else {
-            echo "<pre>";
-            echo var_dump($statusText);
-            echo "</pre>";
-            echo "Status Code: ".$statusCode;
         }
+
+        //Open file handler.
+        // $fp = fopen($saveTo, 'w+');
+
+        // //If $fp is FALSE, something went wrong.
+        // if ($fp === false) {
+        //     Log::debug('Could not open: '.$saveTo);
+        // }
+        
+        // //Create a cURL handle.
+        // $ch = curl_init($this->upload);
+
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_VERBOSE, 1);
+
+        // curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: curl/7.39.0');
+        // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        
+
+        // //Pass our file handle to cURL.
+        // curl_setopt($ch, CURLOPT_FILE, $fp);
+
+        // //Timeout if the file doesn't download after 8mins.
+        // curl_setopt($ch, CURLOPT_TIMEOUT, 480);
+
+        // //Execute the request.
+        // curl_exec($ch);
+
+        // //If there was an error, throw an Exception
+        // if (curl_errno($ch)) {
+        //     Log::debug(curl_error($ch));
+        // }
+
+        // //Get the HTTP status code.
+        // $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        // $statusText = curl_getinfo($ch);
+
+        // //Close the cURL handler.
+        // curl_close($ch);
+
+        // //Close the file handler.
+        // fclose($fp);
+
+        // if ($statusCode == 200) {
+        //     $this->stream->update([
+        //         'url' => $saveTo
+        //     ]);
+        // } else {
+        //     echo "<pre>";
+        //     echo var_dump($statusText);
+        //     echo "</pre>";
+        //     echo "Status Code: ".$statusCode;
+        // }
 
 
     }
