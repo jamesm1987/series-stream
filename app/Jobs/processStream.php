30b624c8ba8faf7cc9d36e2230b862b05a1fd313
James McCormack <james@wirebox.co.uk>
2021-12-25 15:05:04 +0100

    process job debug

--- app/Jobs/processStream.php
+++ app/Jobs/processStream.php
@@ -58,6 +58,8 @@ class processStream implements ShouldQueue
 
         curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: curl/7.39.0');
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        
 
         //Pass our file handle to cURL.
         curl_setopt($ch, CURLOPT_FILE, $fp);
