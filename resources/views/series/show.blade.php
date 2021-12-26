eafff6d8638441e2ef8ec7f838018c135af05b4e
James McCormack <james@wirebox.co.uk>
2021-12-27 00:42:05 +0100

    fix season show route

--- resources/views/series/show.blade.php
+++ resources/views/series/show.blade.php
@@ -1,16 +1,12 @@
 <x-front-layout>
-    @if (!empty($season))
+    @if (!empty($series))
         <main class="my-2">
             <section class="bg-gradient-to-r from-indigo-700 to-transparent">
                 <div class="max-w-6xl mx-auto m-4 p-2">
                     <div class="flex">
-    
                         <div class="w-8/12">
                             <div class="m-4 p-6">
-                                <h1 class="flex text-white font-bold text-4xl">Season {{ $season->season_number }}</h1>
-                                <div class="flex p-3 text-white space-x-4">
-                                    <span>Series: <strong>{{ $series->name }}</strong></span>
-                                </div>
+                                <h1 class="flex text-white font-bold text-4xl">{{ $series->name }}</h1>
                             </div>
 
                         </div>
@@ -20,24 +16,24 @@
             <section class="max-w-6xl mx-auto bg-gray-200 dark:bg-gray-900 p-2 rounded">
                 <div class="flex justify-between">
                     <div class="w-7/12">
-                        <h1 class="flex text-white font-bold text-xl">Episodes</h1>
+                        <h1 class="flex text-white font-bold text-xl">Seasons</h1>
                         <div class="grid grid-cols-2 md:grid-cols-5 gap-4 mt-4">
-                            @foreach ($season->episodes as $episode)
+                            @foreach ($series->seasons as $season)
                                 <x-movie-card>
-                                    <a href="{{ route('episodes.show', $episode->id) }}">
-                                        <span class="text-white">{{ $episode->episode_number }}</span>
+                                    <a href="{{ route('season.show', [$series->id, $season->id]) }}">
+                                        <span class="text-white">{{ $season->season_number }}</span>
                                     </a>
                                 </x-movie-card>
                             @endforeach
                         </div>
                     </div>
                     <div class="w-4/12">
-                        <h1 class="flex text-white font-bold text-xl">Latest seasons</h1>
+                        <h1 class="flex text-white font-bold text-xl">Latest movies</h1>
                         <div class="grid grid-cols-3 gap-2">
                             @if (!empty($latests))
-                                @foreach ($latests as $lseason)
-                                    <a href="{{ route('seasons.show', [$lseason->series->id, $lseason->id]) }}">
-                                        Season {{ $lseason->season_number }}
+                                @foreach ($latests as $lseries)
+                                    <a href="{{ route('series.show', $lseries->id) }}">
+                                        {{ lseries->name }}
                                     </a>
                                 @endforeach
                             @endif
